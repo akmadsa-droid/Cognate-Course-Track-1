f4d917f6ae00bbb90ccabff853ab32692ad7b0ef
akmadsa-droid <akmadsa@ndtc.edu.ph>
2026-05-19 17:50:26 +0800

    Add files via upload

--- Lab exercise 1/Lab exercise 1.2/lab_exercise_1.2.ino
+++ Lab exercise 1/Lab exercise 1.2/lab_exercise_1.2.ino
@@ -1,28 +1,38 @@
-const int numLEDs = 11;
-int ledPins[numLEDs] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-
 void setup() {
-  for (int i = 0; i < numLEDs; i++) {
-    pinMode(ledPins[i], OUTPUT);
+ 
+  for (int pin = 2; pin <= 12; pin++) {
+    pinMode(pin, OUTPUT);
   }
 }
 
 void loop() {
-  
-  for (int i = 0; i < numLEDs / 2; i++) {
-    digitalWrite(ledPins[i], HIGH);                     
-    digitalWrite(ledPins[numLEDs - 1 - i], HIGH);       
-    delay(150);
-    digitalWrite(ledPins[i], LOW);
-    digitalWrite(ledPins[numLEDs - 1 - i], LOW);
+ 
+  for (int i = 0; i <= 5; i++) {
+    int leftPin = 2 + i;
+    int rightPin = 12 - i;
+    
+    
+    digitalWrite(leftPin, HIGH);
+    digitalWrite(rightPin, HIGH);
+    delay(150); 
+    
+ 
+    digitalWrite(leftPin, LOW);
+    digitalWrite(rightPin, LOW);
   }
 
- 
-  for (int i = numLEDs / 2 - 1; i >= 0; i--) {
-    digitalWrite(ledPins[i], HIGH);
-    digitalWrite(ledPins[numLEDs - 1 - i], HIGH);
+  
+  for (int i = 4; i >= 1; i--) {
+    int leftPin = 2 + i;
+    int rightPin = 12 - i;
+    
+    digitalWrite(leftPin, HIGH);
+    digitalWrite(rightPin, HIGH);
     delay(150);
-    digitalWrite(ledPins[i], LOW);
-    digitalWrite(ledPins[numLEDs - 1 - i], LOW);
+    
+    digitalWrite(leftPin, LOW);
+    digitalWrite(rightPin, LOW);
   }
+  
+  
 }
